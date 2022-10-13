7fd25a5d1b0580ebda1b542ce8ac455d948644a7
Max Renner <75229206+maxrenner101@users.noreply.github.com>
2022-10-13 17:32:08 -0400

    Delete src/main directory

--- src/main/java/me/ghostcodes/minecraft/game/world/Chunk.java
+++
@@ -1,7 +0,0 @@
-package me.ghostcodes.minecraft.game.world;
-
-public class Chunk {
-    public Chunk(int x, int z){
-
-    }
-}
--- src/main/java/me/ghostcodes/util/Function.java
+++
@@ -1,5 +0,0 @@
-package me.ghostcodes.util;
-
-public interface Function<T> {
-    void apply(T t);
-}
