a0b2f41b24259a9a4f600c6eebf6d4cdb521c7a9
Max Renner <75229206+maxrenner101@users.noreply.github.com>
2022-10-20 23:46:47 -0400

    Delete src/main directory

--- src/main/java/me/ghostcodes/Main.java
+++
@@ -1,9 +0,0 @@
-package me.ghostcodes;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Thread thread = new Application();
-        thread.start();
-    }
-}
--- src/main/java/me/ghostcodes/minecraft/world/WorldGenerator.java
+++
@@ -1,7 +0,0 @@
-package me.ghostcodes.minecraft.world;
-
-public class WorldGenerator {
-    public static World createWorld(WorldType type){
-        return new World(type);
-    }
-}
